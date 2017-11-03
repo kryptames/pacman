b7e9960f1a42eb3ec5f9538e77ddfb2f8124054a
Nanthawan <nanthawan.han@ku.th>
2017-11-03 15:21:31 +0700

    can control left right

--- core/src/com/kryptames/game/GameScreen.java
+++ core/src/com/kryptames/game/GameScreen.java
@@ -1,6 +1,7 @@
 package com.kryptames.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -32,7 +33,12 @@ public class GameScreen extends ScreenAdapter{
     }
     
     private void update(float delta) {
-        x += 5;    
+        if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            x -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            x += 10;
+        }
     }
     
 }

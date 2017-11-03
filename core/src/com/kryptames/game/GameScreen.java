f6bc2238636d08e63d662455724e1a656d458697
Nanthawan <nanthawan.han@ku.th>
2017-11-03 15:39:33 +0700

    move four direction

--- core/src/com/kryptames/game/GameScreen.java
+++ core/src/com/kryptames/game/GameScreen.java
@@ -6,19 +6,17 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter{
 	private PacmanGame pacmanGame;
 	private Texture pacmanImg;
+	private Pacman pacman;
 	
-	private int x;
-    private int y;
-	 
     public GameScreen(PacmanGame pacmanGame) {
         this.pacmanGame = pacmanGame;
         pacmanImg = new Texture("pacman.png");
-        x = 100;
-        y = 100;
+        pacman = new Pacman(100,100);
     }
     
     @Override
@@ -28,16 +26,23 @@ public class GameScreen extends ScreenAdapter{
         update(delta);
     	SpriteBatch batch = pacmanGame.batch;
         batch.begin();
-        batch.draw(pacmanImg, x, y);
+        Vector2 pos = pacman.getPosition();
+        batch.draw(pacmanImg, pos.x, pos.y);
         batch.end();
     }
     
     private void update(float delta) {
         if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-            x -= 10;
+        	pacman.move(Pacman.DIRECTION_LEFT);
         }
         if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            x += 10;
+        	pacman.move(Pacman.DIRECTION_RIGHT);
+        }
+        if(Gdx.input.isKeyPressed(Keys.UP)) {
+        	pacman.move(Pacman.DIRECTION_UP);
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+        	pacman.move(Pacman.DIRECTION_DOWN);
         }
     }
     
